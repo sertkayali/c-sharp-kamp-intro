36bef5a65f6cce20daffd443e0f1aa0f6bb2c1e4
sertkayali <61178927+sertkayali@users.noreply.github.com>
2021-11-12 00:01:35 +0300

    Ders Sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Furkan");
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("Ahmet");
+            Console.WriteLine(isimler.Length);
+            Console.WriteLine("Hello World!");
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+    }
+}
